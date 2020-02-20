b33b2a003031e545bb22ed21e36e9d34e0392ccc
League Student <league@iMac-38.attlocal.net>
2020-02-19 16:55:28 -0800

    i learnde how to print and use random numbers to make a digital magic 8 ball

--- src/_05_magic_8_ball/Magic8Ball.java
+++ src/_05_magic_8_ball/Magic8Ball.java
@@ -1,37 +1,67 @@
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
-
 package _05_magic_8_ball;
 
 import java.util.Random;
 
-public class Magic8Ball {
-public static void main(String[] args) {
+import javax.swing.JOptionPane;
+
+public class Magic8Ball {// 1. Make a main method that includes all the steps below….
+	public static void main(String[] args) {
+		// 2. Make a variable that will hold a random number and put a random number
+		// into this variable using "new Random().nextInt(4)"
+
+		int number;
+		number = new Random().nextInt(4);
+		// 3. Print out this variable
+		System.out.println(number);
+		// 4. Get the user to enter a question for the 8 ball
+		String question;
+		question = JOptionPane.showInputDialog("Ask the magic 8 ball a question!");
+		// 5. If the random number is 0
+		// -- tell the user "Yes"
+		if (number == 0) {
+			JOptionPane.showMessageDialog(null, "yes");
+		}
+		// 6. If the random number is 1
+		// -- tell the user "No"
+		else if (number == 1) {
+			JOptionPane.showMessageDialog(null, "no");
+		}
+		// 7. If the random number is 2
+		// -- tell the user "Maybe you should ask Google?"
+		else if (number == 2) {
+			JOptionPane.showMessageDialog(null, "Maybe you should ask google?");
+		}
+		// 8. If the random number is 3
+		// -- write your own answer
+		else if (number == 3) {
+			JOptionPane.showMessageDialog(null, "Absouloutely!");
+		}
+	}
+}
 	
-}
-	// 1. Make a main method that includes all the steps below….
 
-	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
+	
 
-	// 3. Print out this variable
+	
+	
 
-	// 4. Get the user to enter a question for the 8 ball
+	
 
-	// 5. If the random number is 0
+	
 
-	// -- tell the user "Yes"
+	
 
-	// 6. If the random number is 1
+	
+	
+	
 
-	// -- tell the user "No"
+	
 
-	// 7. If the random number is 2
+	
 
-	// -- tell the user "Maybe you should ask Google?"
 
-	// 8. If the random number is 3
 
-	// -- write your own answer
 
-}
---
+++ src/_06_lottery_numbers/LotteryNumbers.java
@@ -0,0 +1,16 @@
+package _06_lottery_numbers;
+
+import java.util.Random;
+
+public class LotteryNumbers {
+public static void main(String[] args) {
+	int number;
+	number = new Random().nextInt(10);
+	int numberTwo;
+	numberTwo = new Random().nextInt(10);
+	number = new Random().nextInt(10);
+	number = new Random().nextInt(10);
+	number = new Random().nextInt(10);
+	
+}
+}
